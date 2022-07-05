d3cccc227c7e3ce52e6dfa10664b82d846f7a905
Ali Alshaikh <ali@aalshaikh.com>
2022-07-05 12:44:17 +0300

    Simple Client - initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cvar-client';
+  title = 'CVaR Client';
+  selectedFile_Name="";
+  selectedConf=0.99;
+  isProg=false;
+  isDone=false;
+  isError=false;
+  resultVal="";
+
+  errorMsg="";
+  
+  constructor(private http: HttpClient) { 
+    
+  }
+
+
+validate=(inVal:any)=>['Backspace','.'].includes(inVal.key) || inVal.key.match("[0123456789]")!==null;
+
+fileSelected(f:any){
+  if(f[0].name)
+  this.selectedFile_Name=f[0].name;
+}
+
+submit(myfilecontrol:HTMLInputElement,cutoffInput:HTMLInputElement){
+
+  this.selectedConf=Number(cutoffInput.value);
+  
+  this.isDone=false;this.isError=false;
+  let is_input_error=this.validateAgain();
+  if(!is_input_error && myfilecontrol.files && myfilecontrol.files.length>0)
+  {
+      this.isProg=true;
+      let url='https://cvar-service-sbz36w5qoq-ey.a.run.app';
+
+      const formData = new FormData();
+      //console.log(this.selectedConf.toString())
+      formData.append('file',myfilecontrol.files[0],myfilecontrol.files[0].name);
+      formData.append('cutoff',this.selectedConf.toString());
+      this.http.post(url,formData).subscribe({
+        next:(v:any)=>{
+          this.resultVal=v.toLocaleString("en-US");;
+          this.isDone=true;
+          this.isProg=false;
+          //console.log(v);
+        }
+        ,error:(er)=>{
+          this.isProg=false;
+          this.isError=true;
+          this.errorMsg="An error occured. Please check your Excel file";
+        }
+      });
+  }
+
+}
+
+validateAgain(){
+  this.errorMsg="";
+  if(!Number(this.selectedConf))
+  {
+    this.errorMsg="The confidence value is not correct"
+  }
+  else if (Number(this.selectedConf)<0 || Number(this.selectedConf)>=1)
+  {
+    this.errorMsg="The confidence value should be between 0 and 1"
+  }
+  else if (this.selectedFile_Name==""){
+    this.errorMsg="No Excel file is selected";
+  }
+  return this.isError=this.errorMsg!="";
+
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,MatIconModule
+    ,MatButtonModule,MatInputModule,MatProgressSpinnerModule,FormsModule
+    ,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
